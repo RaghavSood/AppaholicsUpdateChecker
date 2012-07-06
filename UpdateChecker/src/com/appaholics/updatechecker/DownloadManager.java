9b09347e52059c786c181112466677fb6df1d106
Raghav Sood <raghavsood@appaholics.in>
2012-07-06 12:39:30 +0530

    API 2 committed, removed test project
    New features of API 2 added. Test project removed.

--- UpdateChecker/src/com/appaholics/updatechecker/DownloadManager.java
+++ UpdateChecker/src/com/appaholics/updatechecker/DownloadManager.java
@@ -1,12 +1,9 @@
-/**
- * @author: Raghav Sood
- * @date: Jul 5, 2012
- */
 package com.appaholics.updatechecker;
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -19,20 +16,54 @@ import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.util.Log;
 
+/**
+ * DO NOT USE ON YOUR OWN. All calls are handled through UpdateChecker.
+ * 
+ * @author Raghav Sood
+ * @version API 2
+ * @since API 1
+ *
+ */
 public class DownloadManager extends AsyncTask<String, Integer, String>
 {
     private ProgressDialog progressDialog;
-
-
+    private String TAG = "UpdateDownloadManager";
+    private boolean installAfterDownload = true;
+    private boolean downloaded = false;
     private Context mContext;
     
+    /**
+     * Constructor for the Download Manager. DO NOT USE ON YOUR OWN. All calls are through UpdateChecker
+     * 
+     * @param context Activity context
+     * @since API 1
+     */
     public DownloadManager(Context context)
     {
+    	this(context, true);
+    }
+    
+    /**
+     * Constructor for the download manager. DO NOT USE ON YOUR OWN. All calls are handled through UpdateChecker.
+     * 
+     * @param context Activity context
+     * @param installAfterDownload true if you want to install immediately after download, false otherwise
+     * @since API 2
+     */
+    public DownloadManager(Context context, boolean installAfterDownload)
+    {
         mContext = context;
+        this.installAfterDownload = installAfterDownload;
     }
 
-    public boolean isOnline()
+    /**
+     * Checks to see if we have an active internet connection
+     * @return true if online, false otherwise
+     * @since API 1
+     */
+    private boolean isOnline()
     {
         try
         {
@@ -45,44 +76,62 @@ public class DownloadManager extends AsyncTask<String, Integer, String>
         }
     }
 
-        @Override
-        protected String doInBackground(String... sUrl) {
-            try {
-                URL url = new URL(sUrl[0]);
-                URLConnection connection = url.openConnection();
-                connection.connect();
-                // this will be useful so that you can show a typical 0-100% progress bar
-                int fileLength = connection.getContentLength();
+    /**
+     * Downloads the update file in a background task
+     * 
+     * @since API 1
+     */
+    @Override
+    protected String doInBackground(String... sUrl) {
+    	if(isOnline())
+    	{
+    		try {
+    			URL url = new URL(sUrl[0]);
+    			URLConnection connection = url.openConnection();
+    			connection.connect();
+    			// this will be useful so that you can show a typical 0-100% progress bar
+    			int fileLength = connection.getContentLength();
+    			
+    			// download the file
+    			InputStream input = new BufferedInputStream(url.openStream());
+    			OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/appupdate.apk");
+    			
+    			byte data[] = new byte[1024];
+    			long total = 0;
+    			int count;
+    			while ((count = input.read(data)) != -1) {
+    				total += count;
+    				// publishing the progress....
+    				publishProgress((int) (total * 100 / fileLength));
+    				output.write(data, 0, count);
+            	}
 
-                // download the file
-                InputStream input = new BufferedInputStream(url.openStream());
-                OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/appupdate.apk");
+    			output.flush();
+    			output.close();
+    			input.close();
+           	} catch (IOException e) {
+           		Log.e(TAG, "There was an IOException when downloading the update file");
+           	}
+    	}
+    	return null;
+    }
 
-                byte data[] = new byte[1024];
-                long total = 0;
-                int count;
-                while ((count = input.read(data)) != -1) {
-                    total += count;
-                    // publishing the progress....
-                    publishProgress((int) (total * 100 / fileLength));
-                    output.write(data, 0, count);
-                }
-
-                output.flush();
-                output.close();
-                input.close();
-            } catch (Exception e) {
-            }
-            return null;
-        }
- // end of class DownloadManager()
-
+    /**
+     * Updates our progress bar with the download information
+     * 
+     * @since API 1
+     */
     @Override
     protected void onProgressUpdate(Integer... changed)
     {
         progressDialog.setProgress(changed[0]);
     }
 
+    /**
+     * Sets up the progress dialog to notify user of download progress
+     * 
+     * @since API 1
+     */
     @Override
     protected void onPreExecute()
     {
@@ -93,15 +142,36 @@ public class DownloadManager extends AsyncTask<String, Integer, String>
         progressDialog.show();
     }
 
+    /**
+     * Dismissed progress dialog, calls install() if installAfterDownload is true
+     * 
+     * @since API 1
+     */
     @Override
     protected void onPostExecute(String result)
     {
         progressDialog.dismiss();
-        String filepath = Environment.getExternalStorageDirectory().getPath() + "/appupdate.apk";
-        Uri fileLoc = Uri.fromFile(new File(filepath));
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(fileLoc, "application/vnd.android.package-archive");
-        mContext.startActivity(intent);
-
+        downloaded = true;
+        if(installAfterDownload)
+        {
+        	install();
+        }
+    }
+    
+    /**
+     * Launches an Intent to install the apk update.
+     * 
+     * @since API 2
+     */
+    public void install()
+    {
+    	if(downloaded)
+    	{
+    		String filepath = Environment.getExternalStorageDirectory().getPath() + "/appupdate.apk";
+        	Uri fileLoc = Uri.fromFile(new File(filepath));
+        	Intent intent = new Intent(Intent.ACTION_VIEW);
+        	intent.setDataAndType(fileLoc, "application/vnd.android.package-archive");
+        	mContext.startActivity(intent);
+    	}
     }
 }
--- UpdateChecker/src/com/appaholics/updatechecker/UpdateChecker.java
+++ UpdateChecker/src/com/appaholics/updatechecker/UpdateChecker.java
@@ -1,7 +1,3 @@
-/*
- * @author: Raghav Sood © 2012
- */
-
 package com.appaholics.updatechecker;
 
 import java.io.BufferedReader;
@@ -10,49 +6,112 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @author Raghav Sood
+ * @version API 2
+ * @since API 1
+ */
 
 public class UpdateChecker {
 
 	private String TAG = "UpdateChecker";
 	private boolean updateAvailable = false;
 	private Context mContext;
-
 	private boolean haveValidContext = false;
+	private boolean useToasts = false;
+	private DownloadManager downloadManager;
 
-	public UpdateChecker(Context context) {
+	/**
+	 * Constructor that only takes the Activity context.
+	 * 
+	 * This constructor sets the toast notification functionality to false. Example call:
+	 * 		UpdateChecker uupdateChecker = new UpdateChecker(this);
+	 * 
+	 * @param context An instance of your Activity's context
+	 * @since API 1
+	 */
+	public UpdateChecker(Context context)
+	{
+		this(context, false);
+	}
+	
+	/**
+	 * Constructor for UpdateChecker
+	 * 
+	 * Example call:
+	 * 		UpdateChecker uupdateChecker = new UpdateChecker(this, false);
+	 * 
+	 * @param context An instance of your Activity's context
+	 * @param toasts True if you want toast notifications, false by default
+	 * @since API 2
+	 */
+	public UpdateChecker(Context context, boolean toasts) {
 		mContext = context;
 		if (mContext != null) {
 			haveValidContext = true;
+			useToasts = toasts;
 		}
 	}
 
+	/**
+	 * Checks for app update by version code.
+	 * 
+	 * Example call:
+	 * 		updateChecker.checkForUpdateByVersionCode("http://www.example.com/version.txt");
+	 * 
+	 * @param url URL at which the text file containing your latest version code is located.
+	 * @since API 1
+	 */
 	public void checkForUpdateByVersionCode(String url) {
-		if (haveValidContext) {
-			int versionCode = getVersionCode();
-			int readCode = 0;
-			if (versionCode >= 0) {
-				try {
-					readCode = Integer.parseInt(readFile(url));
-					// Check if update is available.
-					if (readCode > versionCode) {
-						updateAvailable = true; //We have an update available
+		if(isOnline())
+		{
+			if (haveValidContext) {
+				int versionCode = getVersionCode();
+				int readCode = 0;
+				if (versionCode >= 0) {
+					try {
+						readCode = Integer.parseInt(readFile(url));
+						// Check if update is available.
+						if (readCode > versionCode) {
+							updateAvailable = true; //We have an update available
+						}
+					} catch (NumberFormatException e) {
+						Log.e(TAG, "Invalid number online"); //Something wrong with the file content
 					}
-				} catch (NumberFormatException e) {
-					Log.e(TAG, "Invalid number online"); //Something wrong with the file content
+				
+				} else {
+					Log.e(TAG, "Invalid version code in app"); //Invalid version code
 				}
-
 			} else {
-				Log.e(TAG, "Invalid version code in app"); //Invalid version code
+				Log.e(TAG, "Context is null"); //Context was null
 			}
 		} else {
-			Log.e(TAG, "Context is null"); //Context was null
+			if(useToasts)
+			{
+				makeToastFromString("App update check failed. No internet connection available").show();
+			}
 		}
 	}
 
+	/**
+	 * Get's the version code of your app by the context passed in the constructor
+	 * 
+	 * @return The version code if successful, -1 if not
+	 * @since API 1
+	 */
 	public int getVersionCode() {
 		int code;
 		try {
@@ -60,24 +119,161 @@ public class UpdateChecker {
 					mContext.getPackageName(), 0).versionCode;
 			return code; // Found the code!
 		} catch (NameNotFoundException e) {
-			Log.e(TAG, "Version Code not available"); // There was a problem
-														// with the code
-														// retrieval.
+			Log.e(TAG, "Version Code not available"); // There was a problem with the code retrieval.
+		} catch (NullPointerException e) {
+			Log.e(TAG, "Context is null");
 		}
 
 		return -1; // There was a problem.
 	}
 
+	/**
+	 * Downloads and installs the update apk from the URL
+	 * 
+	 * @param apkUrl URL at which the update is located
+	 * @since API 1
+	 */
 	public void downloadAndInstall(String apkUrl)
 	{
-		DownloadManager downloadManager = new DownloadManager(mContext);
-		downloadManager.execute(apkUrl);
+		if(isOnline())
+		{
+			downloadManager = new DownloadManager(mContext, true);
+			downloadManager.execute(apkUrl);
+		}
+		else {
+			if(useToasts)
+			{
+				makeToastFromString("App update failed. No internet connection available").show();
+			}
+		}
 	}
 	
+	/**
+	 * Must be called only after download(). 
+	 * @since API 2
+	 * @throws NullPointerException Thrown when download() hasn't been called.
+	 */
+	public void install()
+	{
+		downloadManager.install();
+	}
+	
+	/**
+	 * Downloads the update apk, but does not install it
+	 * 
+	 * @param apkUrl URL at which the update is located.
+	 * @since API 2
+	 */
+	public void download(String apkUrl)
+	{
+		if(isOnline())
+		{
+		downloadManager = new DownloadManager(mContext, false);
+		downloadManager.execute(apkUrl);
+		} else {
+			if(useToasts)
+			{
+				makeToastFromString("App update failed. No internet connection available").show();
+			}
+		}
+	}
+	
+	/**
+	 * Should be called after checkForUpdateByVersionCode()
+	 * @return Returns true if an update is available, false if not.
+	 * @since API 1
+	 */
 	public boolean isUpdateAvailable() {
 		return updateAvailable;
 	}
 	
+	/**
+	 * Checks to see if an Internet connection is available
+	 * 
+	 * @return True if connected or connecting, false otherwise
+	 * @since API 2
+	 */
+    public boolean isOnline()
+    {
+    	if(haveValidContext)
+    	{
+    		try
+        	{
+            	ConnectivityManager cm = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            	return cm.getActiveNetworkInfo().isConnectedOrConnecting(); 
+        	}
+        	catch (Exception e)
+        	{
+            	return false;
+        	}
+    	}
+    	return false;
+    }
+	
+    /**
+     * Launches your app's page on Google Play if it exists.
+     * 
+     * @since API 2
+     */
+    public void launchMarketDetails()
+    {
+    	if(haveValidContext)
+    	{
+    		if(hasGooglePlayInstalled())
+    		{
+    			String marketPage = "market://details?id=" + mContext.getPackageName();
+    			Intent intent = new Intent(Intent.ACTION_VIEW);
+    			intent.setData(Uri.parse(marketPage));
+    			mContext.startActivity(intent);
+    		} else {
+    			if(useToasts)
+    			{
+    				makeToastFromString("Google Play isn't installed on your device.").show();
+    			}
+    		}
+    	}
+    }
+    
+    /**
+     * Checks to use if the user's device has Google Play installed
+     * @return true if Google Play is installed, otherwise false
+     * @since API 2
+     */
+    public boolean hasGooglePlayInstalled() {
+        Intent market = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=dummy"));
+        PackageManager manager = mContext.getPackageManager();
+        List<ResolveInfo> list = manager.queryIntentActivities(market, 0);
+
+        if (list != null && list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).activityInfo.packageName.startsWith("com.android.vending") == true) {
+                    return true;
+                }
+            }
+         }
+        return false;
+    }
+    
+    /**
+     * Makes a toast message with a short duration from the given text.
+     * @param text The text to be displayed by the toast
+     * @return The toast object.
+     * @since API 2
+     */
+    @SuppressLint("ShowToast")
+	public Toast makeToastFromString(String text)
+    {
+    	Toast toast = Toast.makeText(mContext, text, Toast.LENGTH_SHORT);
+    	return toast;
+    }
+    
+    /**
+     * Reads a file at the given URL
+     * 
+     * @param url The URL at which the file is located
+     * @return Returns the content of the file if successful
+     * @since API 1
+     */
 	public String readFile(String url)
 	{
 		String result;
